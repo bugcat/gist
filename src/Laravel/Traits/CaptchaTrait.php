587ed39ce921384ec867e2b0a9f47307f36931d1
z6 <z6@bug.cat>
2020-03-18 20:15:17 +0800

    upd

--- src/Laravel/Traits/CaptchaTrait.php
+++ src/Laravel/Traits/CaptchaTrait.php
@@ -24,6 +24,9 @@ trait CaptchaTrait
             'pswd_reset'  => 'CAPTCHA_PSWD_RESET',
         ],
         
+        //错误信息的键
+        'err_key' => 'validation.invalid_captcha',
+        
         //不需要判断验证码的环境
         'envs_ignored' => ['local', ],
         
@@ -40,14 +43,39 @@ trait CaptchaTrait
     private $captcha_cfg = [];
     
     
-    private function getCaptchaCfg()
+    private function getCaptchaCfg($key = null)
     {
+        if ( empty($key) ) {
+            $cfg = [];
+            foreach ( self::$cfg as $k => $v ) {
+                $cfg[$k] = $this->__mergeCaptchaCfg($k);
+            }
+            return $cfg;
+        } elseif ( isset(self::$cfg[$key]) ) {
+            return $this->__mergeCaptchaCfg($key);
+        } else {
+            return null;
+        }
         return self::$cfg;
     }
     
+    private function __mergeCaptchaCfg($k)
+    {
+        if ( isset($this->captcha_cfg[$k]) ) {
+            if ( is_array(self::$cfg[$k]) ) {
+                return array_merge(self::$cfg[$k], $this->captcha_cfg[$k]);
+            } else {
+                return $this->captcha_cfg[$k];
+            }
+        } else {
+            return self::$cfg[$k];
+        }
+    }
+    
+    
     private function setCaptchaCfg($newcfg = [])
     {
-        $this->$captcha_cfg = $newcfg;
+        $this->captcha_cfg = $newcfg;
     }
     
     /**
@@ -64,12 +92,11 @@ trait CaptchaTrait
     private function __getCaptchaSessionKey($from = '')
     {
         $key = '';
+        $keys_cfg = $this->getCaptchaCfg('keys');
         if ( empty($from) ) {
             $key = self::$cfg['keys']['_pre'];
-        } elseif ( isset($this->$captcha_cfg['keys'][$from]) ) {
-            $key = $this->$captcha_cfg['keys'][$from];
-        } elseif ( isset(self::$cfg['keys'][$from]) ) {
-            $key = self::$cfg['keys'][$from];
+        } elseif ( isset($keys_cfg[$from]) ) {
+            $key = $keys_cfg[$from];
         } else {
             $key = self::$cfg['keys']['_pre'] . $from;
         }
@@ -86,13 +113,11 @@ trait CaptchaTrait
     private function __textCaptcha($key)
     {
         //获取相关配置
-        $setCfg = $this->captcha_cfg['settings'] ?? [];
-        $config = array_merge(self::$cfg['settings'], $setCfg);
-        
+        $settings = $this->getCaptchaCfg('settings');
         //初始化验证码
         $captcha = new Captcha();
-        $vcode = $captcha->gnrt($config);
-        $sensitive = $this->captcha_cfg['sensitive'] ?? self::$cfg['sensitive'];
+        $vcode = $captcha->gnrt($settings);
+        $sensitive = $this->getCaptchaCfg('sensitive');
         $vcode = $sensitive ? $vcode : Str::lower($vcode);
         //存储验证码
         $hasher = new Hasher;
@@ -101,5 +126,68 @@ trait CaptchaTrait
         //输出验证码
         $captcha->img();
         exit;
+    }
+    
+    
+    /**
+     * 判断验证码是否正确
+     *
+     * @param  array  $info 验证码数据 一维数组
+     * @param  string $from 来源控制器
+     * @param  mix    $rtn  返回数据方式
+     * @return bool||\Illuminate\Contracts\Validation\Validator
+     */
+    public function captchaValidator($info, $from, $rtn = null)
+    {
+        $err_key = $this->getCaptchaCfg('err_key');
+        $messages = is_string($rtn) ? [$err_key => $rtn] : [];
+        $validator = Validator::make($info, [
+            key($info) => ['bail', 'required', 'string', function() use ($from) {
+                //判断环境
+                $envs = $this->getCaptchaCfg('envs_ignored');
+                if ( in_array(config('app.env'), $envs) ) {var_dump('test1');
+                    return true;
+                }
+                //获取session键
+                $skey = $this->__getCaptchaSessionKey($from);
+                return $this->__takeCheckCaptcha(func_get_args(), $skey);
+            },],
+        ], $messages);
+        if ( true === $rtn ) {
+            return $validator;
+        }
+        return $validator->validate();
+    }
+    
+    /**
+     * 验证码判断
+     *
+     * @param $value
+     * @param $skey
+     * @return bool
+     */
+    private final function __takeCheckCaptcha($args, $skey)
+    {
+        $bool = true;
+        list($attribute, $value, $fail) = $args;
+        
+        if ( empty($skey) || ! session()->has($skey) ) {
+            $bool = false;
+        } else {
+            $vcode = session()->get($skey);
+            $sensitive = $this->getCaptchaCfg('sensitive');
+            $value = $sensitive ? $value : Str::lower($value);
+            $res = Hasher::check($value, $vcode);
+            //  if verify pass,remove session
+            if ( $res ) {
+                session()->remove($skey);
+            }
+            $bool = $res;
+        }
+        if ( $bool ) {
+            return true;
+        }
+        $err_key = $this->getCaptchaCfg('err_key');
+        return $fail(trans($err_key));
     }
 }
