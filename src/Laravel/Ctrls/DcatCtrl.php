6ba6f9f864c2680b087103f98ef2a8ca1f90ec05
z6 <z6@bug.cat>
2020-12-13 22:32:23 +0800

    Add DcatAdmin CtrlBase

---
+++ src/Laravel/Ctrls/DcatCtrl.php
@@ -0,0 +1,95 @@
+<?php namespace Bugcat\Gist\Laravel\Ctrls;
+
+use Dcat\Admin\Controllers\AdminController;
+use Dcat\Admin\{Form, Grid, Show, Admin};
+use Bugcat\Gist\Laravel\Traits\CtrlTrait;
+
+class DcatCtrl extends AdminController
+{
+    use CtrlTrait;
+    
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Example controller';
+    
+    protected $table = null; //数据库表名
+    protected $model = null; //模型对象
+    
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //parent::__construct();
+        if ( ! empty($this->table) ) {
+            $this->model = $this->m($this->table);
+        }
+    }
+    
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid($this->model);
+
+        $grid->column('id', 'ID')->sortable();
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show($this->model->findOrFail($id));
+
+        $show->field('id', 'ID');
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form($this->model);
+
+        $form->display('id', 'ID');
+        $form->display('created_at', __('Created At'));
+        $form->display('updated_at', __('Updated At'));
+
+        return $form;
+    }
+    
+    //执行JS
+    protected function js($script = '')
+    {
+        Admin::script($script);
+    }
+    
+    //执行Css
+    protected function css($style = '')
+    {
+        Admin::style($style);
+    }
+}
